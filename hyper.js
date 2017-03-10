b68d51f34e5d303f669cf786213d5de9db92db60
Nicolas Simon <nicolas1.simon@epita.fr>
2017-03-10 07:54:50 +0000

    Update dotfiles

--- hyper.js
+++ hyper.js
@@ -4,7 +4,7 @@ module.exports = {
     fontFamily: 'Consolas, "Lucida Console", monospace',
 
     shell: 'C:\\Windows\\System32\\bash.exe',
-    shellArgs: ['-c', 'cd ~ && /bin/zsh'],
+    shellArgs: ['-c', 'cd ~; /bin/zsh'],
 
     env: {
       'DISPLAY': ':0',
@@ -12,6 +12,7 @@ module.exports = {
 
     bell: false,
     copyOnSelect: false,
+    cursorBlink: true,
     windowPosition: [150, 100],
     windowSize: [1000, 600],
   },
