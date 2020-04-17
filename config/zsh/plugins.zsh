a1a978f005924da3687160bae742cb52c9a03998
Nicolas Simon <contact@nsimon.fr>
2020-04-17 15:43:44 +0200

    Update config

--- config/zsh/plugins.zsh
+++ config/zsh/plugins.zsh
@@ -1,5 +1,7 @@
 source ${0:a:h}/prompt.zsh
 source ${0:a:h}/history.zsh
+source ${0:a:h}/completion.zsh
+source ${0:a:h}/setopt.zsh
 source ${0:a:h}/z.sh
 source ${0:a:h}/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ${0:a:h}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
